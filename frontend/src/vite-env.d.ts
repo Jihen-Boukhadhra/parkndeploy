76174ae11cacba212d375e68aa5b39205df2c0c5
Jihen BOUKHADHRA <129115407+Jihen-Boukhadhra@users.noreply.github.com>
2026-01-21 11:10:26 +0100

    Update vite-env.d.ts

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_VERSION: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly npm_package_version: string
+  }
+}
